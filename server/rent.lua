71657fb5b6e897250e906ea3a78f87c5fac5c553
lazicdev <109192302+lazicdev@users.noreply.github.com>
2026-01-14 21:50:14 +0100

    Update rent.lua

--- server/rent.lua
+++ server/rent.lua
@@ -1,194 +1,34 @@
 ESX = exports["es_extended"]:getSharedObject()
 
-RegisterNetEvent('naplata:gradfuto')
-AddEventHandler('naplata:gradfuto', function()
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local futopare = Config.Vozilo1Naplata
+RegisterNetEvent('rent:naplati')
+AddEventHandler('rent:naplati', function(locationName, vehicleIndex)
+    local src = source
+    local xPlayer = ESX.GetPlayerFromId(src)
+    local vozilo = Config.Vozila[vehicleIndex]
+    local cena = vozilo.cena
+    
+    if not vozilo then return end
+    
+    local imaNovca = false
+    
     if Config.Placanje == 'cash' then
-        if xPlayer.getMoney() >= futopare then
-            xPlayer.removeMoney(futopare)
-            TriggerClientEvent('lazic:rentgrad1', source)
-        else 
-            TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Nemate dovoljno novca'})
-        end
+        imaNovca = xPlayer.getMoney() >= cena
     elseif Config.Placanje == 'bank' then
-        if xPlayer.getAccount('bank').money >= futopare then
-            xPlayer.removeAccountMoney('bank', futopare)
-            TriggerClientEvent('lazic:rentgrad1', source)
-        else 
-            TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Nemate dovoljno novca'})
-        end
+        imaNovca = xPlayer.getAccount('bank').money >= cena
     end
-end)
-
-RegisterNetEvent('naplata:gradsultan')
-AddEventHandler('naplata:gradsultan', function()
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local sultanpare = Config.Vozilo2Naplata
-    if Config.Placanje == 'cash' then
-        if xPlayer.getMoney() >= sultanpare then
-            xPlayer.removeMoney(sultanpare)
-            TriggerClientEvent('lazic:rentgrad2', source)
-        else 
-            TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Nemate dovoljno novca'})
+    
+    if imaNovca then
+        if Config.Placanje == 'cash' then
+            xPlayer.removeMoney(cena)
+        elseif Config.Placanje == 'bank' then
+            xPlayer.removeAccountMoney('bank', cena)
         end
-    elseif Config.Placanje == 'bank' then
-        if xPlayer.getAccount('bank').money >= sultanpare then
-            xPlayer.removeAccountMoney('bank', sultanpare)
-            TriggerClientEvent('lazic:rentgrad2', source)
-        else 
-            TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Nemate dovoljno novca'})
-        end
-    end
-end)
-
-RegisterNetEvent('naplata:gradfaggio')
-AddEventHandler('naplata:gradfaggio', function()
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local faggiopare = Config.Vozilo3Naplata
-    if Config.Placanje == 'cash' then
-        if xPlayer.getMoney() >= faggiopare then
-            xPlayer.removeMoney(faggiopare)
-            TriggerClientEvent('lazic:rentgrad3', source)
-        else 
-            TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Nemate dovoljno novca'})
-        end
-    elseif Config.Placanje == 'bank' then
-        if xPlayer.getAccount('bank').money >= faggiopare then
-            xPlayer.removeAccountMoney('bank', faggiopare)
-            TriggerClientEvent('lazic:rentgrad3', source)
-        else 
-            TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Nemate dovoljno novca'})
-        end
-    end
-end)
-
--- SANDY --
-
-RegisterNetEvent('naplata:sandyfuto')
-AddEventHandler('naplata:sandyfuto', function()
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local futopare = Config.Vozilo1Naplata
-    if Config.Placanje == 'cash' then
-        if xPlayer.getMoney() >= futopare then
-            xPlayer.removeMoney(futopare)
-            TriggerClientEvent('lazic:rentsandy1', source)
-        else 
-            TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Nemate dovoljno novca'})
-        end
-    elseif Config.Placanje == 'bank' then
-        if xPlayer.getAccount('bank').money >= futopare then
-            xPlayer.removeAccountMoney('bank', futopare)
-            TriggerClientEvent('lazic:rentsandy1', source)
-        else 
-            TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Nemate dovoljno novca'})
-        end
-    end
-end)
-
-RegisterNetEvent('naplata:sandysultan')
-AddEventHandler('naplata:sandysultan', function()
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local sultanpare = Config.Vozilo2Naplata
-    if Config.Placanje == 'cash' then
-        if xPlayer.getMoney() >= sultanpare then
-            xPlayer.removeMoney(sultanpare)
-            TriggerClientEvent('lazic:rentsandy2', source)
-        else 
-            TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Nemate dovoljno novca'})
-        end
-    elseif Config.Placanje == 'bank' then
-        if xPlayer.getAccount('bank').money >= sultanpare then
-            xPlayer.removeAccountMoney('bank', sultanpare)
-            TriggerClientEvent('lazic:rentsandy2', source)
-        else 
-            TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Nemate dovoljno novca'})
-        end
-    end
-end)
-
-RegisterNetEvent('naplata:sandyfaggio')
-AddEventHandler('naplata:sandyfaggio', function()
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local faggiopare = Config.Vozilo3Naplata
-    if Config.Placanje == 'cash' then
-        if xPlayer.getMoney() >= faggiopare then
-            xPlayer.removeMoney(faggiopare)
-            TriggerClientEvent('lazic:rentsandy3', source)
-        else 
-            TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Nemate dovoljno novca'})
-        end
-    elseif Config.Placanje == 'bank' then
-        if xPlayer.getAccount('bank').money >= faggiopare then
-            xPlayer.removeAccountMoney('bank', faggiopare)
-            TriggerClientEvent('lazic:rentsandy3', source)
-        else 
-            TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Nemate dovoljno novca'})
-        end
-    end
-end)
-
--- PALETO --
-
-RegisterNetEvent('naplata:paletofuto')
-AddEventHandler('naplata:paletofuto', function()
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local futopare = Config.Vozilo1Naplata
-    if Config.Placanje == 'cash' then
-        if xPlayer.getMoney() >= futopare then
-            xPlayer.removeMoney(futopare)
-            TriggerClientEvent('lazic:rentpaleto1', source)
-        else 
-            TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Nemate dovoljno novca'})
-        end
-    elseif Config.Placanje == 'bank' then
-        if xPlayer.getAccount('bank').money >= futopare then
-            xPlayer.removeAccountMoney('bank', futopare)
-            TriggerClientEvent('lazic:rentpaleto1', source)
-        else 
-            TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Nemate dovoljno novca'})
-        end
-    end
-end)
-
-RegisterNetEvent('naplata:paletosultan')
-AddEventHandler('naplata:paletosultan', function()
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local sultanpare = Config.Vozilo2Naplata
-    if Config.Placanje == 'cash' then
-        if xPlayer.getMoney() >= sultanpare then
-            xPlayer.removeMoney(sultanpare)
-            TriggerClientEvent('lazic:rentpaleto2', source)
-        else 
-            TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Nemate dovoljno novca'})
-        end
-    elseif Config.Placanje == 'bank' then
-        if xPlayer.getAccount('bank').money >= sultanpare then
-            xPlayer.removeAccountMoney('bank', sultanpare)
-            TriggerClientEvent('lazic:rentpaleto2', source)
-        else 
-            TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Nemate dovoljno novca'})
-        end
-    end
-end)
-
-RegisterNetEvent('naplata:paletofaggio')
-AddEventHandler('naplata:paletofaggio', function()
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local faggiopare = Config.Vozilo3Naplata
-    if Config.Placanje == 'cash' then
-        if xPlayer.getMoney() >= faggiopare then
-            xPlayer.removeMoney(faggiopare)
-            TriggerClientEvent('lazic:rentpaleto3', source)
-        else 
-            TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Nemate dovoljno novca'})
-        end
-    elseif Config.Placanje == 'bank' then
-        if xPlayer.getAccount('bank').money >= faggiopare then
-            xPlayer.removeAccountMoney('bank', faggiopare)
-            TriggerClientEvent('lazic:rentpaleto3', source)
-        else 
-            TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Nemate dovoljno novca'})
-        end
+        
+        TriggerClientEvent('rent:spawnVehicle', src, locationName, vozilo.model)
+    else
+        TriggerClientEvent('mythic_notify:client:SendAlert', src, { 
+            type = 'error', 
+            text = 'Nemate dovoljno novca' 
+        })
     end
 end)
